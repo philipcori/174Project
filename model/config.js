e5abf7ee7f537701af8c8ff8ab4e8a42986a8ab4
philipcori <philip.cori@gmail.com>
2019-11-01 22:40:35 -0700

    add db connection

---
+++ model/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    hrPool: {
+      user: 'pcori',
+      password: '174Project',
+      connectString: 'dbserver.engr.scu.edu/db11g',
+      poolMin: 10,
+      poolMax: 10,
+      poolIncrement: 0
+    }
+  };
